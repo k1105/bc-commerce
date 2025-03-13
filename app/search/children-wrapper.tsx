aad200f93c35daf609bc852cac2086af07e3651d
k1105 <kanapon1105@yahoo.co.jp>
2025-03-13 21:59:52 +0900

    suspenseで囲む

--- app/search/children-wrapper.tsx
+++ app/search/children-wrapper.tsx
@@ -1,10 +1,18 @@
-'use client';
+"use client";
 
-import { useSearchParams } from 'next/navigation';
-import { Fragment } from 'react';
+import {useSearchParams} from "next/navigation";
+import {Fragment, Suspense} from "react";
 
 // Ensure children are re-rendered when the search query changes
-export default function ChildrenWrapper({ children }: { children: React.ReactNode }) {
+export default function ChildrenWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const searchParams = useSearchParams();
-  return <Fragment key={searchParams.get('q')}>{children}</Fragment>;
+  return (
+    <Suspense fallback="<div>loading...</div>">
+      <Fragment key={searchParams.get("q")}>{children}</Fragment>
+    </Suspense>
+  );
 }
